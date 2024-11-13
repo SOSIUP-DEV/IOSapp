00ab0bf8b054861decd53ece02112313533f1b8e
Bayram Ilgar Aydogan <ilgaraydogan@icloud.com>
2024-11-13 21:07:38 +0300

    Proje başlangıç

--- SOSIUP/SOSIUPApp.swift
+++ SOSIUP/SOSIUPApp.swift
@@ -6,12 +6,36 @@
 //
 
 import SwiftUI
+import Firebase
+import FirebaseAuth
+import FirebaseFirestore
+import FirebaseStorage
+import FirebaseMessaging
+import FirebaseCore
 
 @main
 struct SOSIUPApp: App {
+    
+    @UIApplicationDelegateAdaptor(AppDelegate.self) var delegate
+    
     var body: some Scene {
         WindowGroup {
             ContentView()
         }
     }
 }
+
+
+// MARK -- App Delegate
+
+
+class AppDelegate: NSObject, UIApplicationDelegate {
+  func application(_ application: UIApplication,
+                   didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
+    FirebaseApp.configure()
+    return true
+  }
+}
+
+
+
