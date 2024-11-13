29525d422802ce09e083949e5be02554c6274092
Bayram Ilgar Aydogan <ilgaraydogan@icloud.com>
2024-11-13 20:57:59 +0300

    Initial Commit

---
+++ SOSIUP/SOSIUPApp.swift
@@ -0,0 +1,17 @@
+//
+//  SOSIUPApp.swift
+//  SOSIUP
+//
+//  Created by Bayram Ilgar Aydoğan on 13.11.2024.
+//
+
+import SwiftUI
+
+@main
+struct SOSIUPApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ SOSIUPTests/SOSIUPTests.swift
@@ -0,0 +1,17 @@
+//
+//  SOSIUPTests.swift
+//  SOSIUPTests
+//
+//  Created by Bayram Ilgar Aydoğan on 13.11.2024.
+//
+
+import Testing
+@testable import SOSIUP
+
+struct SOSIUPTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
